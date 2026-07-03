d61e1b2d364c945633bcb47e90d22b17da452f2f
PracticumTutor <PracticumTutor@gmail.com>
2026-07-03 17:34:13 -0400

    New update project for my project

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,14 @@
+import "./footer.css";
+
+function footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__name-and-surname">Developed by Name Surname</p>
+      <div className="footer__date">
+        <p className="footer__year">2022</p>
+      </div>
+    </footer>
+  );
+}
+
+export default footer;
